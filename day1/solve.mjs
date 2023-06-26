c924a151ce1178805ba0bf6a3b3ebb7f2476cb98
João Pedro <jpedro1580@gmail.com>
2023-06-26 11:41:36 -0300

    Formatting

--- day1/solve.mjs
+++ day1/solve.mjs
@@ -1,20 +1,20 @@
-import { readIntegersFromFile } from "../helpers.mjs"
-const data = readIntegersFromFile("./input.txt")
+import { readIntegersFromFile } from "../helpers.mjs";
+const data = readIntegersFromFile("./input.txt");
 const part1 = data.reduce((acc, cur, idx, arr) => {
-    return cur > arr[idx - 1 > 0 ? idx - 1 : 0] ? acc + 1 : acc
-}, 0)
+    return cur > arr[idx - 1 > 0 ? idx - 1 : 0] ? acc + 1 : acc;
+}, 0);
 
-console.log("Part 1:", part1)
+console.log("Part 1:", part1);
 
-let part2 = 0
+let part2 = 0;
 
 for (let i = 0; i < data.length - 4; i += 1) {
-    const curr = data[i]
-    const next = data.at(i + 3)
+    const curr = data[i];
+    const next = data.at(i + 3);
 
     if (next > curr) {
-        part2 += 1
+        part2 += 1;
     }
 }
 
-console.log("Part 2:", part2)
+console.log("Part 2:", part2);
--- day3/solve.mjs
+++ day3/solve.mjs
@@ -1,52 +1,61 @@
-import { readFileSync } from "node:fs"
+import { readFileSync } from "node:fs";
 
-const data = readFileSync("./input.txt", "utf8").split("\n").filter(Boolean)
+const data = readFileSync("./input.txt", "utf8").split("\n").filter(Boolean);
 
 function buildFrequencyMap(data) {
     return data.reduce((acc, cur) => {
         for (let i = 0; i < cur.length; i++) {
             if (!acc[i]) {
-                acc[i] = {}
+                acc[i] = {};
             }
-            acc[i][cur[i]] ? acc[i][cur[i]]++ : acc[i][cur[i]] = 1
+            acc[i][cur[i]] ? acc[i][cur[i]]++ : (acc[i][cur[i]] = 1);
         }
-        return acc
-    }, [])
+        return acc;
+    }, []);
 }
 
 function reduceToMostCommonBits(acc, cur) {
-    return acc + (cur["1"] >= cur["0"] ? "1" : "0")
+    return acc + (cur["1"] >= cur["0"] ? "1" : "0");
 }
 
 function reduceToLeastCommonBits(acc, cur) {
-    return acc + (cur["1"] >= cur["0"] ? "0" : "1")
+    return acc + (cur["1"] >= cur["0"] ? "0" : "1");
 }
 
 function bitFlip(byteString) {
-    return byteString.split("").map(b => b === "0" ? "1" : "0").join("")
+    return byteString
+        .split("")
+        .map((b) => (b === "0" ? "1" : "0"))
+        .join("");
 }
 
-const bitFrequencyMap = buildFrequencyMap(data)
-const gammaRate = bitFrequencyMap.reduce(reduceToMostCommonBits, "")
-const epsilonRate = bitFlip(gammaRate)
-console.log("part1: ", parseInt(gammaRate, 2) * parseInt(epsilonRate, 2))
+const bitFrequencyMap = buildFrequencyMap(data);
+const gammaRate = bitFrequencyMap.reduce(reduceToMostCommonBits, "");
+const epsilonRate = bitFlip(gammaRate);
+console.log("part1: ", parseInt(gammaRate, 2) * parseInt(epsilonRate, 2));
 
 function filterByFrequencyWindow(data, bitFrequencyMapBuilder, reducer) {
-    let result = [...data]
-    let i = 0
+    let result = [...data];
+    let i = 0;
     while (result.length > 1) {
-        const currentFrequencyMap = bitFrequencyMapBuilder(result)
-        const currentMaskBit = currentFrequencyMap.reduce(reducer, "")[i]
-        result = result.filter(byte => byte[i] === currentMaskBit)
-        ++i
+        const currentFrequencyMap = bitFrequencyMapBuilder(result);
+        const currentMaskBit = currentFrequencyMap.reduce(reducer, "")[i];
+        result = result.filter((byte) => byte[i] === currentMaskBit);
+        ++i;
     }
-    return result
+    return result;
 }
 
-const o2rate = filterByFrequencyWindow(data, buildFrequencyMap, reduceToMostCommonBits)
-const co2rate = filterByFrequencyWindow(data, buildFrequencyMap, reduceToLeastCommonBits)
+const o2rate = filterByFrequencyWindow(
+    data,
+    buildFrequencyMap,
+    reduceToMostCommonBits
+);
+const co2rate = filterByFrequencyWindow(
+    data,
+    buildFrequencyMap,
+    reduceToLeastCommonBits
+);
 
 const part2 = parseInt(o2rate, 2) * parseInt(co2rate, 2);
-console.log("part 2:", part2)
-
-
+console.log("part 2:", part2);
